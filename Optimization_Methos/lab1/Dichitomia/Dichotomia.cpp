973a019eac258e1cea4751a7f054064c6c69fc3e
chegevarisys <90518356+chegevarisys@users.noreply.github.com>
2021-09-11 18:40:01 +0300

    added test

--- Optimization_Methos/lab1/Dichitomia/Dichotomia.cpp
+++ Optimization_Methos/lab1/Dichitomia/Dichotomia.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-
-
+#include "./test1.cpp"
+#include "../Helpers/test2.cpp"
 
 int main()
 {
+	auto a = A();
+	std::cout<<"---------------------------------"<<std::endl;
+	auto b = B();
 	return 0;
 }
